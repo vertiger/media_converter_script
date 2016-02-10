3abe6faa5c1b493d4f92a57016814f0806646065
Lars Schmohl <lars.schmohl@gmail.com>
2016-02-11 00:09:10 +0100

    add open directory dialog and recursive search

--- media.py
+++ media.py
@@ -1,11 +1,29 @@
+#!/usr/bin/python
+
 import enzyme
+import fnmatch
 import glob
+import pygtk, gtk
 import shutil
 import subprocess
 import os
 
+dialog = gtk.FileChooserDialog(title='Please select a directory with movies', action=gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER, buttons=(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
 
-for mfile in glob.iglob("/run/user/1000/gvfs/smb-share:server=ds415plus.local,share=video/*/*/*.mkv"):
+response = dialog.run()
+if response == gtk.RESPONSE_OK:
+    print dialog.get_filename(), 'selected'
+else:
+    quit()
+dirname = dialog.get_filename()
+dialog.destroy()
+
+matches = []
+for root, dirnames, filenames in os.walk(dirname):
+    for filename in fnmatch.filter(filenames, '*.mkv'):
+        matches.append(os.path.join(root, filename))
+
+for mfile in matches:
     try:
         with open(mfile, 'rb') as f:
             mkv = enzyme.MKV(f)
@@ -17,14 +35,14 @@ for mfile in glob.iglob("/run/user/1000/gvfs/smb-share:server=ds415plus.local,sh
     print("convert file \"{}\"".format(mfile))
     print(mkv)
     audio_tracks = ['{}'.format(idx) for idx, _ in enumerate(mkv.audio_tracks, start=1)]
-    audio_arg = "--audio-fallback av_aac -a {} -E {}".format(','.join(audio_tracks), ','.join(['copy' for _ in audio_tracks]))
+    audio_arg = "--audio-copy-mask aac --audio-fallback av_aac -a {} -E {}".format(','.join(audio_tracks), ','.join(['copy' for _ in audio_tracks]))
     subtitle_tracks = ['{}'.format(idx) for idx, _ in enumerate(mkv.subtitle_tracks, start=1)]
     if subtitle_tracks:
         subtitle_arg = "-s {}".format(','.join(subtitle_tracks))
     else:
         subtitle_arg = ""
     target_file = os.path.join('/tmp', os.path.basename(mfile))
-    command = "HandBrakeCLI -i \"{}\" -o \"{}\" -O --preset=\"High Profile\" --h264-level 5.2 --x264-preset slow {} {} --keep-display-aspect".format(
+    command = "HandBrakeCLI -i \"{}\" -o \"{}\" -O --preset=\"High Profile\" --h264-level 4.0 --x264-preset slow {} {} --keep-display-aspect".format(
         mfile, target_file, audio_arg, subtitle_arg)
     print(command)
     try:
